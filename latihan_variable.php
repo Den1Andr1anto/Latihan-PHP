f5317b1b6335d278c3b80005fd71fe6df33d6d67
DENI ANDRIANTO <70729593+Den1Andr1anto@users.noreply.github.com>
2020-10-03 01:10:43 +0700

    Add files via upload

---
+++ latihan_variable.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "DENI ANDRIANTO";
+$address ="CIMAHI SELATAN";
+$phone = "081324966152";
+
+echo "NAMA : " .$name. "<br><br><br>";
+echo "ALAMAT : " .$address. "<br><br><br>";
+echo "NO Telp : " .$phone. "<br><br><br>";
+
+?>
