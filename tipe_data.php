9bfa69528108e5d0bcbd195c4f6cf0dbb88d0b30
DENI ANDRIANTO <70729593+Den1Andr1anto@users.noreply.github.com>
2020-10-03 10:13:23 +0700

    Add files via upload

---
+++ tipe_data.php
@@ -0,0 +1,25 @@
+<?php
+
+// string
+$name = "DENI ANDRIANTO";
+$name2 = "NISA GUSMUNIKA";
+
+//integer / int
+
+$number1 = 23;
+$number2 = 14;
+
+//boolean
+
+$better = true;
+$better2 = false;
+
+echo "NAMA AYAH : " .$name. "<br><br>";
+echo "NAMA MAMAH : " .$name2. "<br><br>";
+
+echo "Tanggal lahir ayah saya " .$number2. " Maret 1993 <br><br>"; 
+echo "Tanggal lahir mamah saya " .$number1. " AGUSTUS 1994 <br><br>";
+echo $better. "<br>";
+echo $better2. "<br>";
+
+?>
