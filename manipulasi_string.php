fb472af156d1e9d4d8d4086090c7ad041cc14afc
DENI ANDRIANTO <70729593+Den1Andr1anto@users.noreply.github.com>
2020-10-04 01:41:24 +0700

    Add files via upload

---
+++ manipulasi_string.php
@@ -0,0 +1,11 @@
+<?php
+
+// menghitung jumlah karakter 
+
+$message = "Belajar course PHP di dumbways.id sangat asik loh";
+$message_toUpper = strtoupper($message);
+echo $message. "<br><br>";
+
+echo $message_toUpper;
+
+?>
